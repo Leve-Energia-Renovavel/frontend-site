2999cd7352e801e49983aa5c62645f06349599a4
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-01-19 16:49:41 -0300

    done succes page, ready to begin styling

--- src/app/register/forms/success-form/SuccessForm.jsx
+++ src/app/register/forms/success-form/SuccessForm.jsx
@@ -1,14 +1,21 @@
 import SuccessFormProgress from "./SuccessFormProgress";
 import SuccessFormTitle from "./SuccessFormTitle";
 import { FormContainer, FormHeader } from "./styles";
+import DefaultButton from "@/app/pages/components/utils/buttons/DefaultButton";
+import { useRouter } from "next/navigation";
 
 export default function SuccessForm() {
+    const router = useRouter()
+
     return (
         <FormContainer>
             <FormHeader>
                 <SuccessFormTitle />
                 <SuccessFormProgress />
             </FormHeader>
+            <div style={{ margin: '0 auto' }}>
+                <DefaultButton variant="contained" text={"Acessar a minha conta"} onClick={() => router.push(`/`)} />
+            </div>
         </FormContainer>
     );
 }
