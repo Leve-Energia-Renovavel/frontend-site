bb4940539b7a071c5773f73b9b5d39dde3095f33
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-02-27 17:13:44 -0300

    fixing chart width

--- src/app/pages/components/charts/HistorySpendingChart.jsx
+++ src/app/pages/components/charts/HistorySpendingChart.jsx
@@ -8,7 +8,7 @@ export default function HistorySpendingChart() {
             xAxis={[{ scaleType: 'band', label: "2024", position: 'left', data: ['01/2024', '02/2024', '03/2024', '04/2024', '05/2024'] }]}
             yAxis={[{ label: 'kWh', position: 'left' }]}
             series={[{ data: [300, 500, 1200, 600, 900] }]}
-            width={600}
+            width={500}
             height={300}
         />
     );
