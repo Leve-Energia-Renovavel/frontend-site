15f210d8d86c3ce0e3488f8336059599107eeb47
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-03-07 20:59:01 -0300

    fix config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
     transpilePackages: ['@mui/x-charts'],
     compiler: {
         styledComponents: true
+    },
+    env: {
+        NEXT_PUBLIC_SIGNUP_BASE_URL: process.env.NEXT_PUBLIC_SIGNUP_BASE_URL,
+        NEXT_PUBLIC_FETCH_CNPJ: process.env.NEXT_PUBLIC_FETCH_CNPJ,
     }
 
 }
