f402eaacf4a432372452076d0060833813e940cb
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-04-15 11:41:55 -0300

    fix connection

--- src/app/pages/components/connection/ConnectionMain.jsx
+++ src/app/pages/components/connection/ConnectionMain.jsx
@@ -1,14 +1,24 @@
 "use client"
+import { clearBrowserData } from '@/app/utils/browser/BrowserUtils'
 import Cookies from 'js-cookie'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { notFound, useRouter, useSearchParams } from 'next/navigation'
+
 export default function ConnectionMain() {
+
     const router = useRouter()
     const search = useSearchParams()
+
+    clearBrowserData()
+
     const token = search.get("token")
+
     if (token) {
         Cookies.set('accessToken', token)
         router.push('/dashboard')
+    } else {
+        notFound()
     }
+
     return (
         <>
         </>
