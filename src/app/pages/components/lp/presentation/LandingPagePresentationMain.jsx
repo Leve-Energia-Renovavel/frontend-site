18d8a1b3e608114d2be700a2946131594b97c1b6
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-05-08 14:47:19 -0300

    changed texts to infojson

--- src/app/pages/components/lp/presentation/LandingPagePresentationMain.jsx
+++ src/app/pages/components/lp/presentation/LandingPagePresentationMain.jsx
@@ -5,6 +5,7 @@ import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Image from 'next/image';
+import infoJson from "../../../../../../public/lp-presentation-info.json";
 import percentageImage from "../../../../../resources/icons/large/icone-background-percentage-white.svg";
 import sunImage from "../../../../../resources/icons/small/ellipse-small.webp";
 import companyCardImage from "../../../../../resources/img/large/leve-confraternizacao-image-large.webp";
@@ -13,36 +14,30 @@ import homeCardImage from "../../../../../resources/img/large/leve-familia-brinc
 import bannerImage from '../../../../../resources/img/large/leve-pai-e-filho-image-large.webp';
 import { LandingPageContainer as Container, FifthSectionButton, FifthSectionContainer, FifthSectionContent, FourthSectionImagesContainer, FourthSectionImagesContent, LandingPageMainBanner as MainBanner, MainBannerButton, LandingPageMainContent as MainContent, MoreAboutLeveFooter, SecondSectionBox, SecondSectionBoxesContainer, SecondSectionButton, SecondSectionContainer, SecondSectionContent, SecondSectionContentTitleContainer, SecondSectionTitleContainer, SixthSectionBanner, SixthSectionBox, SixthSectionBoxesContainer, SixthSectionContainer, SixthSectionContent, SixthSectionContentHeader, SixthSectionContentManifest, ThirdSectionContainer } from './styles';
 
-
+import { useRouter } from 'next/navigation';
 import BoxesContainer from '../../home/HomeBoxes';
 import BrandsContainer from '../../home/HomeBrands';
 import TutorialContainer from '../../home/HomeTutorial';
 
 export default function LandingPagePresentationMain() {
 
-    const texts = {
-        together: "Junto com você, a nossa energia ",
-        takeCare: "cuida do planeta",
-        yourPocketToo: " - e também do seu bolso",
-        lighterBill: "Deixe sua conta de luz mais leve com até ",
-        twentyPercent: "20% de economia",
-        everyMonth: " todos os meses.",
-        simulate: "Faça uma simulação",
-        moreAboutLeve: "Mais sobre a Leve",
-        ourSolutions: "Nossas soluções",
-        weHelpYou: "Nós te ajudamos a ",
-        saveTwentyPercent: "economizar até 20% na conta de luz",
-        everyMonthTo: " todos os meses, para que você tenha dinheiro extra para viver momentos especiais ao lado de quem é importante para você.",
-        forYourHouse: "Para a sua residência",
-        forYourCompany: "Para o seu comércio ou empresa",
-        energizeTheWorld: "Queremos energizar o mundo para um futuro mais leve e renovável",
-        manifestFirst: "Na jornada por um mundo mais sustentável, surgimos para compartilhar energia limpa e conectar pessoas.",
-        manifestSecond: "Fazemos isso de forma calorosa, no calor que é transmitido e recebido em cada conexão que criamos.",
-        manifestThird: "Levamos economia, e com ela, novas possibilidades para você e sua família.",
-        manifestFourth: "Estamos redefinindo os padrões da indústria, oferecendo soluções que não só atendem, mas superam as expectativas.",
-        manifestFifth: "Juntos, estamos construindo um amanhã mais luminoso, onde a energia é muito mais do que um recurso: é uma ferramenta para o progresso.",
-        manifestSixth: "O seu e o nosso. Juntos e leves.",
+    const router = useRouter()
+
+    const handleScroll = () => {
+        const element = document.getElementById('howLeveWorks');
+        if (element) {
+            window.scrollTo({ top: element.offsetTop - 60, behavior: 'smooth' });
+        } else {
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+
+        }
     }
+
+    const handleGoHome = () => {
+        router.push("/")
+    }
+
+    const texts = infoJson
 
     return (
         <Container>
@@ -52,10 +47,11 @@ export default function LandingPagePresentationMain() {
                     <h2>{texts.lighterBill}<span className='underlined'>{texts.twentyPercent}</span>{texts.everyMonth}</h2>
                     <MainBannerButton
                         className='mainButton'
+                        onClick={() => handleGoHome()}
                         endIcon={<ArrowForwardIcon />}>
                         <span>{texts.simulate}</span>
                     </MainBannerButton>
-                    <MoreAboutLeveFooter>
+                    <MoreAboutLeveFooter onClick={() => handleScroll()}>
                         <ArrowCircleDownIcon className='arrowIcon' />
                         <p className='moreAboutLeve'>{texts.moreAboutLeve}</p>
                     </MoreAboutLeveFooter>
@@ -76,6 +72,7 @@ export default function LandingPagePresentationMain() {
                         <h4>Venha ser Leve com a energia do futuro</h4>
                         <SecondSectionButton
                             className='desktopButton'
+                            onClick={() => handleGoHome()}
                             endIcon={<ArrowForwardIcon />}>
                             <span>{texts.simulate}</span>
                         </SecondSectionButton>
@@ -94,6 +91,7 @@ export default function LandingPagePresentationMain() {
                         })}
                         <SecondSectionButton
                             className='mobileButton'
+                            onClick={() => handleGoHome()}
                             endIcon={<ArrowForwardIcon />}>
                             <span>{texts.simulate}</span>
                         </SecondSectionButton>
@@ -108,11 +106,11 @@ export default function LandingPagePresentationMain() {
             </ThirdSectionContainer>
 
             <FourthSectionImagesContainer>
-                <FourthSectionImagesContent image={homeCardImage}  >
+                <FourthSectionImagesContent image={homeCardImage} onClick={() => handleGoHome()}>
                     <h6 variant="subtitle1">{texts.forYourHouse}</h6>
                     <ArrowCircleRightOutlinedIcon className='arrowIcon' />
                 </FourthSectionImagesContent>
-                <FourthSectionImagesContent invertedBox={true} image={companyCardImage}>
+                <FourthSectionImagesContent invertedBox={true} image={companyCardImage} onClick={() => handleGoHome()}>
                     <h6 variant="subtitle1">{texts.forYourCompany}</h6>
                     <ArrowCircleRightOutlinedIcon className='arrowIcon' />
                 </FourthSectionImagesContent>
@@ -121,8 +119,9 @@ export default function LandingPagePresentationMain() {
 
             <FifthSectionContainer >
                 <FifthSectionContent image={percentageImage}>
-                    <h6>Clique no botão abaixo e faça uma simulação real da sua economia:</h6>
+                    <h6>{texts.callToActionTitle}</h6>
                     <FifthSectionButton
+                        onClick={() => handleGoHome()}
                         endIcon={<ArrowForwardIcon />}>
                         <span>{texts.simulate}</span>
                     </FifthSectionButton>
