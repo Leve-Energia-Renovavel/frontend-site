582f881f2f03d49eef8a59d76c924fd5e0349c98
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-05-09 18:11:40 -0300

    updated path for landing page

--- src/app/lp/apresentacao/page.jsx
+++ src/app/lp/apresentacao/page.jsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-const LandingPagePresentationMain = dynamic(() => import('@/app/pages/components/lp/presentation/LandingPagePresentationMain'), { ssr: false });
+const LandingPagePresentationMain = dynamic(() => import('@/app/pages/components/lp/apresentacao/LandingPagePresentationMain'), { ssr: false });
 
 export default function LandingPagePresentationPage() {
     return (
