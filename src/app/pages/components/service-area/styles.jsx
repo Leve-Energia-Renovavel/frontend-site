7179bb50bb45cf591b94600948afd74ed2501021
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-05-22 15:41:02 -0300

    finish service area optimization

--- src/app/pages/components/service-area/styles.jsx
+++ src/app/pages/components/service-area/styles.jsx
@@ -1,17 +1,118 @@
 
 import styled from "@emotion/styled";
-import { newBackground } from "../../styles";
+import { background, newBackground } from "../../styles";
 
 export const ServiceAreaContainer = styled.div`
     display: flex;
     flex-direction: column;
     background-color: ${newBackground.white};
 
+    min-height: 600px;
+
     max-width: 1366px;
-    margin: 74px auto 0 auto; 
+    margin: 74px auto; 
     padding: 0 3rem;
 
     @media (max-width: 600px) {
-      padding: 0;
+      padding: 0 1rem;
     }
+`
+export const ServiceAreaContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1000px;
+
+    margin: 0 auto; 
+    padding: 1rem;
+
+    .title {
+        font-family: "Graphie";
+        font-size: 34px;
+        line-height: 37px;
+        font-weight: 600;
+        color: ${newBackground.orange};
+
+        margin-top: 1rem;
+    }
+
+    .headerContainer {
+        margin: 2rem 0;
+
+        .MuiOutlinedInput-root {
+            border-color: ${newBackground.white};
+        }
+
+        .MuiInputLabel-root {
+            color: ${newBackground.orange};
+        }
+        .MuiOutlinedInput-input {
+        // styles for the user input text
+        font-family: "Graphie";
+        font-size: 17px;
+        font-weight: 500;
+        color: ${newBackground.orange};
+        }
+
+        &:hover .MuiOutlinedInput-notchedOutline {
+            border-color: ${newBackground.orange};
+        }
+    }
+
+
+`
+export const ServiceAreaHeaderContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+
+    .stateSelect {
+        width: 300px;
+    }
+    
+    .countyTextField {
+        width: 400px;
+    }
+
+    @media (max-width: 600px) {
+      flex-wrap: wrap;
+
+        .stateSelect {
+            width: 300px;
+        }
+        .countyTextField {
+            width: 300px;
+        }
+    }
+`
+export const AreaContent = styled.div`
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+    .card {
+        display: flex;
+        flex-direction: column;
+        border-radius: 15px;
+        border: 1px solid ${background.grey};
+
+        padding: 2rem;
+    }
+
+    .stateTitle {
+        font-family: "Graphie";
+        font-size: 27px;
+        font-weight: 600;
+        color: ${newBackground.orange};
+
+        margin-bottom: 2rem;
+    }
+
+    .areaNotFound {
+        font-family: "Graphie";
+        font-size: 21px;
+        line-height: 24px;
+        font-weight: 500;
+        color: ${newBackground.green};
+    }
+
 `
