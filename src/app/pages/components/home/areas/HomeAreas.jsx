1310eb0129c7bda5576c2cfd806b7dd8cf1de18a
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-10-29 14:45:24 -0300

    merged from feature/LEVE-113-lps-map

--- src/app/pages/components/home/areas/HomeAreas.jsx
+++ src/app/pages/components/home/areas/HomeAreas.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import brasilMap from '../../../../../resources/img/large/area-atendimento-leve-large.svg';
-import { AreaListContainer, HomeAreasContainer, LegendIcon, MapLegend, MapLegendContainer } from './styles';
+import { HomeAreasContainer } from './styles';
 
 export default function HomeAreas() {
 
@@ -11,19 +11,6 @@ export default function HomeAreas() {
         <HomeAreasContainer>
             <h3 className='areasTitle'>Veja as áreas atendidas</h3>
             <Image alt='Área de atendimento da Leve Energias' src={brasilMap} loading='lazy' className='map' />
-            <MapLegendContainer>
-                <MapLegend hasFactory={true}>
-                    <LegendIcon className='legendIcon' />
-                    <p className='legendDescription'>Estados com usinas da Leve Energia</p>
-                </MapLegend>
-                <MapLegend hasFactory={false}>
-                    <LegendIcon className='legendIcon' />
-                    <p className='legendDescription'>Próximos estados a receber usinas</p>
-                </MapLegend>
-            </MapLegendContainer>
-            <AreaListContainer>
-                <p className='areaList'><span className='highlighted' onClick={() => router.push(`/municipios-atendidos`)}>Clique aqui</span> e veja a lista completa de municípios atendidos</p>
-            </AreaListContainer>
         </HomeAreasContainer>
     )
 }
