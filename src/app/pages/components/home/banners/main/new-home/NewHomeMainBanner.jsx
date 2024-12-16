f44b704582be44fa2d15d8ebd574ccd9a32b1f36
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-12-16 12:13:54 -0300

    fix home page copy

--- src/app/pages/components/home/banners/main/new-home/NewHomeMainBanner.jsx
+++ src/app/pages/components/home/banners/main/new-home/NewHomeMainBanner.jsx
@@ -11,7 +11,7 @@ export default function NewHomeMainBanner() {
       <Container className="leveHomeMainBannerContainer">
         <HomeMain className='leveHomeMainContainer'>
           <h1 className='homeMainTitle'>Economia de até <span className='highlighted'>20% na conta de luz</span> com energia limpa.</h1>
-          <h2 className='homeMainSubtitle'>Solução 100% digital da Leve Energia que garante economia mensal na sua conta de luz a partir da produção de energia limpa.</h2>
+          <h2 className='homeMainSubtitle'>Solução 100% digital da Leve Energia que garante economia mensal na sua conta de luz a partir da produção de energia limpa</h2>
 
           <DescriptionContainer className='leveHomeMainBannerDescriptionContainer'>
             <Image src={checkIcon} alt='checkIcon' className='checkIcon' />
