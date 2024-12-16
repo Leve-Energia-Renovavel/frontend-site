5b8e61fa4fda70c6693f0b677326aa26e4a9aad6
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2024-12-16 12:22:43 -0300

    adjust copy and clean code

--- src/app/pages/components/home/banners/main/new-home/NewHomeMainBanner.jsx
+++ src/app/pages/components/home/banners/main/new-home/NewHomeMainBanner.jsx
@@ -12,7 +12,7 @@ export default function NewHomeMainBanner() {
       <Container className="leveHomeMainBannerContainer">
         <HomeMain className='leveHomeMainContainer'>
           <h1 className='homeMainTitle'>Economia de até <span className='highlighted'>20% na conta de luz</span> com energia limpa.</h1>
-          <h2 className='homeMainSubtitle'>Solução 100% digital da Leve Energia que garante economia mensal na sua conta de luz a partir da produção de energia limpa</h2>
+          <h2 className='homeMainSubtitle'>Solução 100% digital da Leve Energia que garante economia mensal na sua conta de luz a partir da produção de energia limpa.</h2>
 
           <DescriptionContainer className='leveHomeMainBannerDescriptionContainer'>
             <Image src={checkIcon} alt='checkIcon' className='checkIcon' />
@@ -23,7 +23,6 @@ export default function NewHomeMainBanner() {
         </HomeMain>
 
         <NewHomeMainBaloon>
-          {/* <p>Você economizou <span className='hihglighted'>R$42,50</span> nesse mês! Com a Leve, você já ajudou a reduzir 1.450kg da emissão de CO2 que equivale a <span className='hihglighted'>4 árvores plantadas.</span></p> */}
           <p>Você economizou <span className='hihglighted'>R$42,50</span> nesse mês!</p>
           <p>Sua ajuda ao meio ambiente equivale a <span className='hihglighted'>4 árvores plantadas.</span></p>
         </NewHomeMainBaloon>
