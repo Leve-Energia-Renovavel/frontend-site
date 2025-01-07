40246422ab3f27eb97fbae8a9c1728031aee0b38
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2025-01-07 14:38:54 -0300

    wip

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import Messages from "./pages/components/messages/Messages";
 
 import { Partytown } from '@builder.io/partytown/react';
 import Script from "next/script";
-
+import Hotjar from "./pages/components/hotjar/Hotjar";
 
 const WhatsAppFAB = dynamic(() => import('./pages/components/fabWhatsapp/WhatsappFAB'), { ssr: false });
 const Footer = dynamic(() => import('./pages/components/footer/Footer'), { ssr: false });
@@ -64,30 +64,7 @@ export default function RootLayout({ children }) {
       <Head>
         <Partytown>
           <Script id="hotjar" type="text/partytown">
-            {`(function (h, o, t, j, a, r) {
-                        h.hj =
-                        h.hj ||
-                        function () {
-                            // eslint-disable-next-line prefer-rest-params
-                            (h.hj.q = h.hj.q || []).push(arguments);
-                        };
-                        h._hjSettings = { hjid: ${hotjarId}, hjsv: 6 };
-                        a = o.getElementsByTagName("head")[0];
-                        r = o.createElement("script");
-                        r.async = 1;
-                        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                        a.appendChild(r);
-                    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");`}
-          </Script>
-          <Script id="gtm" type="text/partytown">
-            {`
-            (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer',${gtmId});
-            `}
-
+            <Hotjar hotjarId={hotjarId}/>
           </Script>
         </Partytown>
       </ Head>
