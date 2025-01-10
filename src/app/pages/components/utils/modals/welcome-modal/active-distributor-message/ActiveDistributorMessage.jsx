c780f18b0fe9707622508f05788a14d7a3778e9d
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2025-01-10 10:37:23 -0300

    fix copy in active distributor messages

--- src/app/pages/components/utils/modals/welcome-modal/active-distributor-message/ActiveDistributorMessage.jsx
+++ src/app/pages/components/utils/modals/welcome-modal/active-distributor-message/ActiveDistributorMessage.jsx
@@ -6,7 +6,8 @@ export default function ActiveDistributorMessage() {
         <Container className="activeDistributorMessageContainer">
             <h6 className="title">Parabéns, você contratou a Leve!</h6>
             <p className="subtitle">Seus benefícios iniciam em até 60 dias.</p>
-            <p className="descriptionPrimary">Agora, vamos validar suas informações e solicitar que sua distribuidora faça a inclusão do seu imóvel na lista de  consumidora das nossas usinas. Fique de olhos nas atualizações que enviaremos no seu Whastapp e e-mail.</p>
+            <p className="descriptionPrimary">Agora, vamos validar os seus dados e solicitar o cadastro do seu imóvel como consumidor das nossas usinas para a sua distribuidora, que deverá emitir a sua primeira conta de luz com nossos créditos de energia em 60 dias.</p>
+            <p className="descriptionPrimary">Nesse período, enviaremos mensagens com atualizações no seu Whatsapp e e-mail. Fique de olho!</p>
 
             <HighlightBox className="highlightBox">
                 <BoltOutlinedIcon className='icon' />
@@ -18,7 +19,7 @@ export default function ActiveDistributorMessage() {
 
             <DiscountBanner className="discountBanner">
                 <CardGiftcardIcon className="icon" />
-                <p className="discountText">Complete seu cadastro agora e resgate desconto extra na sua primeira fatura.</p>
+                <p className="discountText">Completando seu cadastro agora, você garante desconto extra na sua primeira fatura </p>
                 <DiscountValue className="discountValue">
                     <p className="discountValuePercentage">+5%</p>
                     <p className="discount">de desconto</p>
