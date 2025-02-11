9016ecad2c315a334b2687aae0c6aee1d057d051
Marcos Ferreira <marcos.ferreira@leveenergia.com.br>
2025-02-11 10:39:28 -0300

    add public data

--- src/app/hooks/useFetchEmailAndWhatsapp.jsx
+++ src/app/hooks/useFetchEmailAndWhatsapp.jsx
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import dados from '../../../public/dados.json'
 
 const useFetchEmailAndWhatsapp = () => {
-    const [data, setData] = useState("");
+    const [data, setData] = useState(dados);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const finalUrl = `${process.env.NEXT_PUBLIC_FETCH_WHATSAPP_EMAIL}`;
+                const finalUrl = `https://cors-anywhere.herokuapp.com/${process.env.NEXT_PUBLIC_FETCH_WHATSAPP_EMAIL}`;
                 const response = await axios.get(finalUrl);
                 const parsedData = typeof response.data === "string" ? JSON.parse(response.data) : response.data;
 
